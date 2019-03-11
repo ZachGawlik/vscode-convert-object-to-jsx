26096376355ef7981cabae75c5317e111aae5c70
Zach Gawlik <ZachGawlik@users.noreply.github.com>
2019-03-10 19:31:30 -0500

    Rename method in test

--- src/convertObjectToJsx.test.ts
+++ src/convertObjectToJsx.test.ts
@@ -1,82 +1,74 @@
-import convertObjectToJsx from './convertObjectToJsx'
+import convert from './convertObjectToJsx'
 
-describe('convertObjectToJsx', () => {
+describe('convert', () => {
   describe('single-line values', () => {
     test('Converts non-string non-object primitives', () => {
-      expect(convertObjectToJsx(`a: 1`)).toEqual(`a={1}`)
-      expect(convertObjectToJsx(`a: 1,`)).toEqual(`a={1}`)
-      expect(convertObjectToJsx(`  a: 1\n`)).toEqual(`  a={1}\n`)
-      expect(convertObjectToJsx(`  a: null,\n`)).toEqual(`  a={null}\n`)
+      expect(convert(`a: 1`)).toEqual(`a={1}`)
+      expect(convert(`a: 1,`)).toEqual(`a={1}`)
+      expect(convert(`  a: 1\n`)).toEqual(`  a={1}\n`)
+      expect(convert(`  a: null,\n`)).toEqual(`  a={null}\n`)
     })
 
     test('Converts non-template string value', () => {
-      expect(convertObjectToJsx(`a: 'a'`)).toEqual(`a="a"`)
-      expect(convertObjectToJsx(`a: "a"`)).toEqual(`a="a"`)
-      expect(convertObjectToJsx(`trickychars: ' \',"{<}>'`)).toEqual(
+      expect(convert(`a: 'a'`)).toEqual(`a="a"`)
+      expect(convert(`a: "a"`)).toEqual(`a="a"`)
+      expect(convert(`trickychars: ' \',"{<}>'`)).toEqual(
         `trickychars=" ',\\"{<}>"`
       )
     })
 
     test('Converts hyphenated keys', () => {
-      expect(convertObjectToJsx('`data-test`: `purchase-button`')).toEqual(
+      expect(convert('`data-test`: `purchase-button`')).toEqual(
         'data-test={`purchase-button`}'
       )
-      expect(convertObjectToJsx(`"data-test": 'purchase-button'`)).toEqual(
+      expect(convert(`"data-test": 'purchase-button'`)).toEqual(
         `data-test="purchase-button"`
       )
-      expect(convertObjectToJsx(`'data-test': "purchase-button"`)).toEqual(
+      expect(convert(`'data-test': "purchase-button"`)).toEqual(
         `data-test="purchase-button"`
       )
     })
 
     test('Uses jsx shorthand according to option', () => {
-      expect(convertObjectToJsx(`a: true`)).toEqual(`a={true}`)
-      expect(convertObjectToJsx(`a: true`, {useJsxShorthand: true})).toEqual(
-        `a`
-      )
+      expect(convert(`a: true`)).toEqual(`a={true}`)
+      expect(convert(`a: true`, {useJsxShorthand: true})).toEqual(`a`)
     })
 
     test('Converts template literal value', () => {
       /* tslint:disable:no-invalid-template-strings */
-      expect(convertObjectToJsx('a: `string${id}`')).toEqual(
-        'a={`string${id}`}'
-      )
+      expect(convert('a: `string${id}`')).toEqual('a={`string${id}`}')
       /* tslint:enable:no-invalid-template-strings */
     })
 
     test('Converts field with an object value', () => {
-      expect(convertObjectToJsx(`a: {z: 1}`)).toEqual(`a={{z: 1}}`)
-      expect(convertObjectToJsx(`a: {z}`)).toEqual(`a={{z}}`)
+      expect(convert(`a: {z: 1}`)).toEqual(`a={{z: 1}}`)
+      expect(convert(`a: {z}`)).toEqual(`a={{z}}`)
     })
 
     test('Converts ternary value', () => {
-      expect(convertObjectToJsx(`a: someCondition ? 1 : 2`)).toEqual(
+      expect(convert(`a: someCondition ? 1 : 2`)).toEqual(
         `a={someCondition ? 1 : 2}`
       )
-      expect(convertObjectToJsx(`a: 'x' === myString ? 1 : 2`)).toEqual(
+      expect(convert(`a: 'x' === myString ? 1 : 2`)).toEqual(
         `a={'x' === myString ? 1 : 2}`
       )
     })
 
     test('Converts object spread', () => {
-      expect(convertObjectToJsx('  a:1,\n  ...rest,\n  b:2')).toEqual(
+      expect(convert('  a:1,\n  ...rest,\n  b:2')).toEqual(
         `  a={1}\n  {...rest}\n  b={2}`
       )
-      expect(convertObjectToJsx('  a:1,\n  ...rest')).toEqual(
-        `  a={1}\n  {...rest}`
-      )
-      expect(convertObjectToJsx('  ...rest,\n  a:1')).toEqual(
-        `  {...rest}\n  a={1}`
-      )
+      expect(convert('  a:1,\n  ...rest')).toEqual(`  a={1}\n  {...rest}`)
+      expect(convert('  ...rest,\n  a:1')).toEqual(`  {...rest}\n  a={1}`)
     })
 
     test('Converts object spread of function calls', () => {
       expect(
-        convertObjectToJsx('...myPropGetter({field1: value1, field2: value2})')
+        convert('...myPropGetter({field1: value1, field2: value2})')
       ).toEqual('{...myPropGetter({field1: value1, field2: value2})}')
 
       expect(
-        convertObjectToJsx(`
+        convert(`
         ...myPropGetter({
           field1: value1,
           field2: value2
@@ -91,12 +83,10 @@ describe('convertObjectToJsx', () => {
     })
 
     test('Converts from object shorthand', () => {
-      expect(convertObjectToJsx('  a,\n  b:b')).toEqual(`  a={a}\n  b={b}`)
-      expect(convertObjectToJsx('  a:a,\n  b')).toEqual(`  a={a}\n  b={b}`)
-      expect(convertObjectToJsx('  a,\n  b,\n  c')).toEqual(
-        `  a={a}\n  b={b}\n  c={c}`
-      )
-      expect(convertObjectToJsx('  x: "x",\n  a,\n  b')).toEqual(
+      expect(convert('  a,\n  b:b')).toEqual(`  a={a}\n  b={b}`)
+      expect(convert('  a:a,\n  b')).toEqual(`  a={a}\n  b={b}`)
+      expect(convert('  a,\n  b,\n  c')).toEqual(`  a={a}\n  b={b}\n  c={c}`)
+      expect(convert('  x: "x",\n  a,\n  b')).toEqual(
         `  x="x"\n  a={a}\n  b={b}`
       )
     })
@@ -105,7 +95,7 @@ describe('convertObjectToJsx', () => {
   describe('multiline values', () => {
     test('converts multiline ternaries', () => {
       expect(
-        convertObjectToJsx(`
+        convert(`
         fieldWithTernaryValue:
           someReallyLongConditionalCheckReallyReallyReallyLong === undefined
             ? 'option1'
@@ -119,7 +109,7 @@ describe('convertObjectToJsx', () => {
         `)
 
       expect(
-        convertObjectToJsx(`
+        convert(`
         fieldWithTernaryValue:
           someReallyLongConditionalCheckReallyReallyReallyLong === undefined ? 1 : 2,
         `)
@@ -131,7 +121,7 @@ describe('convertObjectToJsx', () => {
 
     test('Handles values with multiline objects', () => {
       expect(
-        convertObjectToJsx(`
+        convert(`
           data: {
             a: {b: {c: 1}},
             meta: {
@@ -160,18 +150,18 @@ describe('convertObjectToJsx', () => {
 
   describe('multiline selections', () => {
     it("preserves entire selection's leading and trailing whitespace", () => {
-      expect(convertObjectToJsx('\n\n  data: 123  \n\n\n')).toEqual(
+      expect(convert('\n\n  data: 123  \n\n\n')).toEqual(
         '\n\n  data={123}  \n\n\n'
       )
       /* TODO: fix this case by reworking convert's leadingNewlines regex
-      expect(convertObjectToJsx('  \n\n  data: 123\n\n\n')).toEqual(
+      expect(convert('  \n\n  data: 123\n\n\n')).toEqual(
         '  \n\n  data={123}  \n\n\n'
       )
       */
     })
 
     it('preserves leading/trailing new lines between entries', () => {
-      expect(convertObjectToJsx('\n\n  data: 123,\n\n  x,\n  y\n')).toEqual(
+      expect(convert('\n\n  data: 123,\n\n  x,\n  y\n')).toEqual(
         '\n\n  data={123}\n\n  x={x}\n  y={y}\n'
       )
     })
